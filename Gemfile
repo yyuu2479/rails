e6a38dbf982bb683657de7e40e987af43123a8c0
yyuu2479 <yyuu2479@gmail.com>
2021-09-07 11:30:47 +0000

    sample_app

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'bootsnap', '>= 1.1.0', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'pry-rails'
 end
 
 group :development do
@@ -70,3 +71,7 @@ gem 'refile-mini_magick'
 gem 'slim-rails'
 # 自動でerbをslimに変換するGem
 gem 'html2slim'
+# コーディング規約に遵守されているかチェックしてくれるGem
+gem 'rubocop-airbnb'
+# 環境変数を管理できるgem
+gem 'dotenv-rails'
--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -1,47 +1,48 @@
 class TodolistsController < ApplicationController
   def new
-    @list = List.new  
+    @list = List.new
   end
-  
+
   def create
     @list = List.new(list_params)
-    
+
     if @list.save
       redirect_to todolist_path(@list.id)
     else
       render :new
     end
   end
-  
+
   def index
+    puts "作成したキー: #{ENV["SECRET_KEY"]}"
+
     @lists = List.all
   end
-  
+
   def show
-   @list = List.find(params[:id])
+    @list = List.find(params[:id])
   end
 
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
     redirect_to todolists_path
   end
-  
+
   # ストロングパラメーター
   private
+
   def list_params
-    params.require(:list).permit(:title,:body,:image)
-    
+    params.require(:list).permit(:title, :body, :image)
   end
-  
 end
